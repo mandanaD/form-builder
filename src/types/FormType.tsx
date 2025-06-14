edf0480aee19848d718465bb914e3dcaa37e8d89
Mandana Dashti <dmandana59@gmail.com>
2025-06-14 23:01:13 +0330

    feat:add chosenPreviewType & checkboxFieldType type

--- src/types/FormType.tsx
+++ src/types/FormType.tsx
@@ -12,5 +12,22 @@ export interface Field {
 
 export interface optionsType {
     definedFields: Field[],
-    setDefinedFields: React.Dispatch<React.SetStateAction<Field[]>>
+    setDefinedFields: React.Dispatch<React.SetStateAction<Field[]>>,
+    setActiveFieldIndex: React.Dispatch<React.SetStateAction<number>>,
+}
+
+export interface chosenPreviewType {
+    setActiveFieldIndex: React.Dispatch<React.SetStateAction<number>>,
+    activeFieldIndex: number,
+    definedFields: Field[],
+    updateOption: (index: number, value: string) => void,
+    removeOption: (index: number) => void,
+    addOption: () => void,
+    deleteField:(index: number) => void,
+}
+export interface checkboxFieldType{
+    field:Field,
+    removeOption:(index: number) => void,
+    addOption: () => void,
+    updateOption: (index: number, value: string) => void,
 }
