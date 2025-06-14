1b71d7803339eb78d8bcc8e656f1185dc20f1e0e
Mandana Dashti <dmandana59@gmail.com>
2025-06-14 23:00:42 +0330

    feat:add option of selectbox functions

--- src/Pages/FormBuilder.tsx
+++ src/Pages/FormBuilder.tsx
@@ -1,24 +1,61 @@
 import FieldOptions from "../Components/FieldOptions.tsx";
 import {useState} from "react";
 import type {Field} from "../types/FormType.tsx";
+import ChosenPreview from "../Components/ChosenPreview.tsx";
 
 const FormBuilder = () => {
     const [definedFields, setDefinedFields] = useState<Field[]>([])
+    const [activeFieldIndex, setActiveFieldIndex] = useState<number>(-1);
+
+    const addOption = () => {
+        const updateOptions = [...definedFields]
+        updateOptions[activeFieldIndex].options.push(`گزینه ${updateOptions[activeFieldIndex].options.length + 1}`)
+        setDefinedFields(updateOptions)
+    }
+
+    const removeOption = (index: number) => {
+        const updateOptions = [...definedFields]
+        const activeOptions = updateOptions[activeFieldIndex].options;
+        activeOptions.splice(index, 1);
+        setDefinedFields(updateOptions)
+    }
+
+    const updateOption = (index: number, value: string) => {
+        const updatedFields = [...definedFields];
+        updatedFields[activeFieldIndex].options[index] = value;
+        setDefinedFields(updatedFields);
+    }
+
+    const deleteField = (index: number) => {
+        const updatedFields = definedFields.filter((_, i) => i !== index);
+        setDefinedFields(updatedFields);
+    }
+
     return (
-        <div>
-            <div>
+        <main
+            className="min-h-screen flex p-2 border sm:border-transparent border-gray-300 rounded-t-4xl sm:rounded-2xl overflow-y-auto">
+            <div
+                className={`flex flex-col flex-1 py-4 rounded-2xl bg-white w-full shadow-[0px_0px_5px_0px_rgba(0,0,0,0.12)] border border-gray-100`}>
+                <header className={"px-4 pb-3 flex items-center border-b border-gray-300/60"}>
+                    <h1 className={"text-sm sm:text-base mb-1.5 text-gray-800 font-medium"}>
+                        ساخت فرم
+                    </h1>
+                </header>
+                <div className="flex-1 flex h-full relative">
+                    <FieldOptions definedFields={definedFields} setActiveFieldIndex={setActiveFieldIndex}
+                                  setDefinedFields={setDefinedFields}/>
+                    <ChosenPreview deleteField={deleteField} updateOption={updateOption} removeOption={removeOption} addOption={addOption} setActiveFieldIndex={setActiveFieldIndex} activeFieldIndex={activeFieldIndex}
+                                   definedFields={definedFields}/>
+                    {
+                        activeFieldIndex && (
+                            <div>
 
-            </div>
-            <div className="flex h-full relative">
-                <FieldOptions definedFields={definedFields} setDefinedFields={setDefinedFields}/>
-                <div>
-
+                            </div>
+                        )
+                    }
                 </div>
-                <div>
-
-                </div>
             </div>
-        </div>
+        </main>
     )
 }
 export default FormBuilder;
