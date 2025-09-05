80b5ccbfbdd09fa61c7cbcbf6093ce2c44fddffd
Mandana Dashti <dmandana59@gmail.com>
2025-09-05 14:16:27 +0330

    feat: add the style and outlet of main container

--- src/Container/Container.tsx
+++ src/Container/Container.tsx
@@ -1,10 +1,11 @@
-import FormBuilder from "../Pages/FormBuilder.tsx";
+import {Outlet} from "react-router";
 
 const Container = () => {
     return (
-        <div>
-            <FormBuilder/>
-        </div>
+        <main
+            className="min-h-screen flex p-2 border sm:border-transparent border-gray-300 rounded-t-4xl sm:rounded-2xl">
+            <Outlet/>
+        </main>
     )
 
 }
