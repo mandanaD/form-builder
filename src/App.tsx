2967310e52051c9d1bfb93c4b1ba0dbcfe73d2e6
Mandana Dashti <dmandana59@gmail.com>
2025-09-05 14:15:35 +0330

    feat: add the formBuilder component route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
 import './App.css'
 import Container from "./Container/Container.tsx";
 import {createBrowserRouter, RouterProvider} from "react-router";
+import FormList from "./Pages/FormList.tsx";
+import FormBuilder from "./Pages/FormBuilder.tsx";
 
 function App() {
     const routes = createBrowserRouter([
         {
             path: "/",
             element: <Container/>,
-        }])
+            children: [
+                {
+                    index: true,
+                    element: <FormList/>
+                },
+                {
+                    path: "create",
+                    element: <FormBuilder/>
+                }
+            ]
+        },
+    ])
 
     return (
         <RouterProvider router={routes}/>
